1a132634754695ccda09c3e95b6e0b50729303a8
Dmitry Kragel <dmitry.kragel@gmail.com>
2023-03-13 09:16:35 +0300

    added css header, promo, about sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,37 @@
 jQuery(function () {
+    const promo = new Swiper("#promo-slider", {
+        loop: true,
+        slidesPerView: 1,
+        // allowTouchMove: false,
+
+        // Navigation arrows
+        navigation: {
+            prevEl: ".js__promo .prev",
+            nextEl: ".js__promo .next",
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: ".js__promo .swiper-scrollbar",
+            enabled: true,
+        },
+    });
+
+    const reviews = new Swiper("#categories-slider", {
+        loop: true,
+        slidesPerView: 5,
+        // allowTouchMove: false,
+
+        // Navigation arrows
+        navigation: {
+            prevEl: ".js-categories .prev",
+            nextEl: ".js-categories .next",
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: ".js-categories .swiper-scrollbar",
+            enabled: true,
+        },
+    });
 });
